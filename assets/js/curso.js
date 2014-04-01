c214fa4656ab2bc75d518d8ce0896aea1fd7bf3f
rosario17 <mrbaltar@yahoo.com>
2014-04-01 13:07:35 -0600

    Ejercicio 5 Condicionales Rosario

---
+++ assets/js/curso.js
@@ -0,0 +1,44 @@
+/*var variable=10-->
+
+alert(variable+10)*/
+//Suma y concatena
+//var a=4
+//var b=7 c=10.5, cadena="prueba"
+//var b=7
+//var c=10.5
+//var resultado=true
+//alert(a+b+c+cadena)
+//alert(c+resultado)
+//var texto1= "hola"
+//var texto2= "hola"
+//var texto3= "adios"
+//alert(texto1!==texto2)
+//alert(texto1>=texto2)
+//alert(texto3>=texto2) // compara letra a letra si la primera es menor 
+//ya la da como menor
+//alert(texto2>=texto3)
+//var mostrarmensaje= false;
+//var mostrado = true;
+//if (!mostrarmensaje   &&  mostrado){
+//	alert('Entro')
+//}
+var numero1 = 5;
+var numero2 = 8;
+ if (numero1 <=numero2)
+ {
+ 	alert("Numero 1 no es mayor que numero 2")
+ }
+ if (numero2>0)
+ {
+ 	alert("Numero 2 es positivo")
+ }
+ if (numero1 <0 || numero1 != 0)
+ {
+ 	alert("Numero 1 es negativo o distinto de cero")
+ }
+ if ((numero1 +1)<=numero2)
+ {
+ 	alert("Incrementar en una unidad el valor de numero 1 no lo hace mayor o igual que numero 2")
+ }
+
+
---
+++ assets/js/curso_5_r.js
@@ -0,0 +1,18 @@
+var numero1 = 5;
+var numero2 = 8;
+ if (numero1 <=numero2)
+ {
+ 	alert("Numero 1 no es mayor que numero 2")
+ }
+ if (numero2>=0)
+ {
+ 	alert("Numero 2 es positivo")
+ }
+ if (numero1 <0 || numero1 != 0)
+ {
+ 	alert("Numero 1 es negativo o distinto de cero")
+ }
+ if ((numero1 +1)<numero2)
+ {
+ 	alert("Incrementar en una unidad el valor de numero 1 no lo hace mayor o igual que numero 2")
+ }
